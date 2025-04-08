bd7aade96f876d86fc3b7ff6bc93cbb723aebdc5
daniilrusanov <daniil12099@gmail.com>
2025-04-08 14:26:38 +0300

    add form constructor

---
+++ src/app/FlowerDeliveryService/FormsConstructor.ts
@@ -0,0 +1,30 @@
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ProductType, productType} from "./ProductType";
+import {durationValidator} from "./validators/DurationValidator";
+import {dateValidator} from "./validators/DateValidator";
+
+export function formsConstructor(
+    type: string,
+    productForm: FormGroup,
+    fb: FormBuilder
+) {
+    const controlsToRemove = [
+        'name',
+        'price'
+    ];
+    controlsToRemove.forEach(control => {
+        if (productForm.contains(control)) {
+            productForm.removeControl(control);
+        }
+    });
+    if (type === productType[0] || type === productType[1] || type === productType[3]) {
+        productForm.addControl('name', fb.control('', Validators.required));
+        productForm.addControl('price', fb.control('', [Validators.required, Validators.min(20)]));
+        if(type === productType[3]) {
+            productForm.addControl('format', fb.control('', Validators.required));
+        }
+    } else if (type === productType[3]) {
+        productForm.addControl('duration', [Validators.required, durationValidator]);
+        productForm.addControl('date', [Validators.required, dateValidator]);
+    }
+}
