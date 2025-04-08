8714a1718f4ca019d5dc7679341218e8af1e0390
daniilrusanov <daniil12099@gmail.com>
2025-04-08 14:25:11 +0300

    add 'adding product option'

--- src/app/view-product-component/view-product-component.page.ts
+++ src/app/view-product-component/view-product-component.page.ts
@@ -12,15 +12,17 @@ import {
 } from '@ionic/angular/standalone';
 import {MyHeaderComponent} from "../my-header/my-header.component";
 import {ProductReadService} from "../FlowerDeliveryService/ProductReadService";
+import {AddProductComponent} from "../components/add-product/add-product.component";
 
 @Component({
   selector: 'app-view-product-component',
   templateUrl: './view-product-component.page.html',
   styleUrls: ['./view-product-component.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, MyHeaderComponent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonLabel, IonButton]
+    imports: [IonContent, CommonModule, FormsModule, MyHeaderComponent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonLabel, IonButton, AddProductComponent]
 })
 export class ViewProductComponentPage implements OnInit {
+  showAddForm: boolean = false;
 
   constructor(public productReadService: ProductReadService) { }
 
@@ -28,4 +30,12 @@ export class ViewProductComponentPage implements OnInit {
     this.productReadService.load();
   }
 
+  addFormShow() {
+    this.showAddForm = true;
+  }
+
+  addProduct($event: any) {
+    this.productReadService.addProduct($event);
+    this.showAddForm = false;
+  }
 }
