5355c4333cc4db0db67279146ccffcabb5b8173c
daniilrusanov <daniil12099@gmail.com>
2025-04-08 14:39:21 +0300

    small update

--- src/app/FlowerDeliveryService/ProductType.ts
+++ src/app/FlowerDeliveryService/ProductType.ts
@@ -1,6 +1,15 @@
-export type ProductType = 'Bouquet'| 'IndoorPlant' | 'GiftSet';
+export type ProductType = 'Bouquet'| 'IndoorPlant' | 'GiftSet' | 'EBouquet';
+export type EProductType = 'png' | 'jpeg' | 'gif';
+
 export const productType: ReadonlyArray<ProductType> = [
     'Bouquet',
     'IndoorPlant',
-    'GiftSet'
+    'GiftSet',
+    'EBouquet'
+]
+
+export const eProductType: ReadonlyArray<EProductType> = [
+    'png',
+    'jpeg',
+    'gif'
 ]
