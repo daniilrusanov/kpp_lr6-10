178d4f1829290ff1d9ad72654ccdbccc7a538de6
daniilrusanov <daniil12099@gmail.com>
2025-04-12 15:57:49 +0300

    add db read service

--- src/app/FlowerDeliveryService/service/ProductBDReadService.ts
+++ src/app/FlowerDeliveryService/service/ProductBDReadService.ts
@@ -2,7 +2,7 @@ import {BehaviorSubject} from "rxjs";
 import {IProduct} from "../basic/IProduct";
 import {Database, onValue, push, ref, remove, set, update} from "@angular/fire/database";
 import {ProductFactory} from "../ProductFactory";
-import {Injectable} from "@angular/core";
+import {Injectable, NgZone} from "@angular/core";
 
 @Injectable({
     providedIn: 'root'
@@ -12,9 +12,11 @@ export class ProductBDReadService {
 
     private productSubject = new BehaviorSubject<IProduct[]>([]);
     product$ = this.productSubject.asObservable();
+    private lastId: number = 0;
 
     constructor(
         private db: Database,
+        private zone: NgZone
     ) {
     }
 
@@ -22,42 +24,41 @@ export class ProductBDReadService {
         const productsRef = ref(this.db, 'products');
         console.log('fetchProducts started');
 
-        onValue(productsRef, (snapshot) => {
-            const data = snapshot.val();
-            const products = data
-                ? Object.entries(data).map((item: any) =>
-                    ProductFactory.createProduct(item)
-                )
-                : [];
-            this.productSubject.next(products);
-            console.log('Fetched products:', products);
+        this.zone.run(() => {
+            onValue(productsRef, (snapshot) => {
+                const data = snapshot.val();
+                const products = data
+                    ? Object.entries(data).map(([key, value]: [string, any]) =>
+                        ProductFactory.createProduct({...value, id: Number(key)})
+                    )
+                    : [];
+
+
+                this.lastId = products.length > 0 ? Math.max(...products.map(p => p.getID())) : 0;
+                console.log("Last ID:", this.lastId);
+                this.productSubject.next(products);
+                console.log('Fetched products:', products);
+            });
         });
     }
 
 
     addProduct(product: IProduct): void {
-        const productsRef = ref(this.db, `products`);
-        const newProductRef = push(productsRef);
-        set(newProductRef, {
+        const id = this.lastId + 1;
+        this.lastId = id;
+
+        const productsRef = ref(this.db, `products/${id}`);
+        set(productsRef, {
             ...product,
-            _id: newProductRef.key,
-            _type: product.getType(),
+            id: id,
+            type: product.getType(),
         });
     }
 
     editProduct(updateProduct: IProduct): void {
-        const productId = updateProduct.getID();
-
-        if (!productId) {
-            console.error('Product ID is undefined!', updateProduct);
-            return;
-        }
-
-        const productsRef = ref(this.db, `products/${productId}`);
+        const productsRef = ref(this.db, `products/${updateProduct.getID()}`);
         update(productsRef, {
-            ...updateProduct,
-            id: productId,
-            type: updateProduct.getType(),
+            ...updateProduct
         });
     }
 
